92bfcf18744c131e97258d21ea1d03707915b95f
bueeny <yangen.gan.evans@gmail.com>
2020-05-03 21:31:42 +0800

    Commit

--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('register/', users_views.register, name = 'register'),
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name = 'logout'),
-    path('profile/', users_views.profile, name = 'profile'), 
+    path('profile/', users_views.profile, name = 'profile'),    
     path('', include('App01.urls')) 
     ##In human terms, if we go to url/app01HomePage/ where do we send them to. If we access app01HomePage, we will go to App01.urls
 ]
--- djangoProject/users/forms.py
+++ djangoProject/users/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.forms import ModelForm
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import SelectDateWidget
+from django.contrib.admin.widgets import AdminDateWidget
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
@@ -13,13 +15,13 @@ class UserRegisterForm(UserCreationForm):
         fields = ['username','email','password1','password2']
 
 
-# Use html 5 built-in date ionput
+# Use html 5 built-in date input
 class DateInput(forms.DateInput):
     input_type = 'date'
-
 
 class UserProfileForm(ModelForm):
     class Meta: 
         model = Profile 
         fields = ['birthday','bio','location']
-        widgets = {'birthday': DateInput()}  
+        # widgets = {'birthday': DateInput(format = '%Y-%m-%d',attrs = {'placeholder': 'Insert Birth Date Here'})}
+        widgets = {'birthday': forms.DateInput(format=('%dd/%mm/%yyyy'  ), attrs={'class':'form-control', 'placeholder':'Select your birthdate', 'type':'date'})}
