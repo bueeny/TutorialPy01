f82c564ae5767191dd5b6b4dcd641765ef0e3425
bueeny <yangen.gan.evans@gmail.com>
2020-05-05 02:19:04 +0800

    Update user profile page

--- djangoProject/users/forms.py
+++ djangoProject/users/forms.py
@@ -2,8 +2,6 @@ from django import forms
 from django.forms import ModelForm
 from django.db import models
 from django.contrib.auth.models import User
-from django.forms import SelectDateWidget
-from django.contrib.admin.widgets import AdminDateWidget
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
@@ -14,14 +12,22 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username','email','password1','password2']
 
+class UserUpdateForm(ModelForm):
 
-# Use html 5 built-in date input
-class DateInput(forms.DateInput):
-    input_type = 'date'
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+# # Use html 5 built-in date input
+# class DateInput(forms.DateInput):
+#     input_type = 'date'
 
 class UserProfileForm(ModelForm):
     class Meta: 
         model = Profile 
-        fields = ['birthday','bio','location']
+        fields = ['image','birthday','bio','location']
         # widgets = {'birthday': DateInput(format = '%Y-%m-%d',attrs = {'placeholder': 'Insert Birth Date Here'})}
-        widgets = {'birthday': forms.DateInput(format=('%dd/%mm/%yyyy'  ), attrs={'class':'form-control', 'placeholder':'Select your birthdate', 'type':'date'})}
+        widgets = {'birthday': forms.DateInput( attrs={'class':'form-control', 'placeholder':'Select your birthdate', 'type':'date'})}
+
+
+
--- djangoProject/users/views.py
+++ djangoProject/users/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegisterForm
-from .forms import UserProfileForm
- 
+from .forms import UserRegisterForm,UserUpdateForm,UserProfileForm
+
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -30,5 +29,24 @@ def register(request):
 
 
 @ login_required # To add in login authentication to access this login route
-def profile(request):
-    return render(request, 'users/profile.html')
+def profile(request): # To fill in form with existing data
+
+    if request.method == 'POST':
+        user_update_form = UserUpdateForm(request.POST, instance = request.user)
+        profile_form = UserProfileForm(request.POST, request.FILES, instance = request.user.profile)
+
+        if user_update_form.is_valid() and profile_form.is_valid():
+            user_update_form.save()
+            profile_form.save()
+            username = user_update_form.cleaned_data.get('username')
+            messages.success(request, f'Profile has been updated successfully for {username}') #message
+            return redirect('profile')
+
+    else:
+        user_update_form = UserUpdateForm(instance = request.user)
+        profile_form = UserProfileForm(instance = request.user.profile)
+
+    # create a context
+    context = {'user_update_form':user_update_form,'profile_form': profile_form }
+
+    return render(request, 'users/profile.html', context)
